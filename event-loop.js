f1c135332233863487e8166d53c173eb5d87026a
maitandat1507 <maitandat1507@gmail.com>
2019-09-11 07:39:02 +0700

    Separate IN EVENT-LOOP and OUT of EVENT-LOOP

--- event-loop.js
+++ event-loop.js
@@ -1,10 +1,19 @@
 const fs = require('fs');
 
+// IN EVENT-LOOP
 setTimeout(() => console.log('Timer 1 finished'), 0);
 setImmediate(() => console.log('Immediate 1 finished'));
 
 fs.readFile('test-file.txt', () => {
+  // IN EVENT-LOOP
   console.log('I/O finished');
+  console.log('-------------------');
+
+  // below is OUT-OFF OF EVENT-LOOP
+  setTimeout(() => console.log('Timer 2 finished'), 0);
+  setTimeout(() => console.log('Timer 3 finished'), 3000);
+  setImmediate(() => console.log('Immediate 2 finished'));
 });
 
+// IN EVENT-LOOP
 console.log('Hello from the top-level code');
